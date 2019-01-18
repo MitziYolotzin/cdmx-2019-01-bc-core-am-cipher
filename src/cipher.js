ae93dd8ddc70a52b736730ff2828568fb0c4d574
MitziYolotzin <ultravisuell@hotmail.com>
2019-01-18 11:24:17 -0600

    correccion de orden de signos en formula

--- src/cipher.js
+++ src/cipher.js
@@ -11,23 +11,10 @@ window.cipher = {
   //encode: () => (),
   //decode: () => ()
 
+  //Formula cifrar
   encode: (numero, texto) => {
     let resultado = texto;
     let upmayus = resultado.toUpperCase();
-    let rescifrado = "";
-
-    for (let i = 0; i < upmayus.length; i++) {
-      let codascii = upmayus.charCodeAt(i);
-      let formula = (codascii + 65 - parseInt(numero)) % 26 + 65;
-      let claveuno = String.fromCharCode(formula);
-      rescifrado += claveuno;
-
-    }
-    return rescifrado;
-  },
-  decode: (numero, texto) => {
-    let resultado = texto;
-    let upmayus   = resultado.toUpperCase();
     let rescifrado = "";
 
     for (let i = 0; i < upmayus.length; i++) {
@@ -38,6 +25,22 @@ window.cipher = {
 
     }
     return rescifrado;
+  },
+
+  //Formula descifrar
+  decode: (numero2, texto2) => {
+    let resultado2 = texto2;
+    let upmayus2   = resultado2.toUpperCase();
+    let rescifrado2 = "";
+
+    for (let i = 0; i < upmayus2.length; i++) {
+      let codascii2 = upmayus2.charCodeAt(i);
+      let formula2 = (codascii2 + 65 - parseInt(numero2)) % 26 + 65;
+      let claveuno2 = String.fromCharCode(formula2);
+      rescifrado2 += claveuno2;
+
+    }
+    return rescifrado2;
   }
 
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@
 const offset = document.getElementById('offset')
 const clave1 = document.getElementById('clave1')
 const bcifrar = document.getElementById('bcifrar')
-
 //const clave2= document.getElementById('clave2')
 const bdescif = document.getElementById ('bdescif')
 const clean = document.getElementById ('clean')
 
-
+//Boton cifrar, guardar valores
 bcifrar.addEventListener('click', () => {
 let numero = offset.value;
 let texto = clave1.value;
@@ -18,14 +17,15 @@ document.getElementById('clave2').value = window.cipher.encode(numero,texto);
 //console.log(numero,texto)
 
 })
-
+//Boton descifrar, guarda valores
 bdescif.addEventListener('click', ()=> {
-    let numero = offset.value;
-    let texto = clave1.value;
-document.getElementById('clave2').value = window.cipher.decode(numero,texto);
+    let numero2 = offset.value;
+    let texto2 = clave1.value;
+document.getElementById('clave2').value = window.cipher.decode(numero2,texto2);
 
 })
 
+//Boton borrar info
 const cleaner = () => {
 location.reload(true);
 };
